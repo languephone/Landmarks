a0482f7f364544fe562d092227f9e38239fd93c2
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2025-03-08 12:08:33 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Daniel Shapiro on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Daniel Shapiro on 3/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
